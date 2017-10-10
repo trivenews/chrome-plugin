d84f9a887b35ba72af8bb545c258a589d45c7b04
dtruel <dtruel@users.noreply.github.com>
2017-10-10 16:40:27 -0500

    fix url

--- settings-data.js
+++ settings-data.js
@@ -1,1 +1,1 @@
-window.EXTENSION_CONFIG = {"buildType":"dev","apiUrl":"http://meteor.trive.news:443/api/","serviceUrl":"http://meteor.trive.news:443/","appType":"chrome-extension","googleAnalytics":"UA-97914674-2"};
+window.EXTENSION_CONFIG = {"buildType":"dev","apiUrl":"https://hive.trive.news:443/api/","serviceUrl":"https://hive.trive.news:443/","appType":"chrome-extension","googleAnalytics":"UA-97914674-2"};
