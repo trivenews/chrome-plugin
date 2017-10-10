d34ff033c140655fcba14367c7166687fd69fd9e
dtruel <dtruel@users.noreply.github.com>
2017-10-10 16:39:15 -0500

    update port to 443

--- settings-data.js
+++ settings-data.js
@@ -1,1 +1,1 @@
-window.EXTENSION_CONFIG = {"buildType":"dev","apiUrl":"http://meteor.trive.news:5000/api/","serviceUrl":"http://meteor.trive.news:5000/","appType":"chrome-extension","googleAnalytics":"UA-97914674-2"};
+window.EXTENSION_CONFIG = {"buildType":"dev","apiUrl":"http://meteor.trive.news:443/api/","serviceUrl":"http://meteor.trive.news:443/","appType":"chrome-extension","googleAnalytics":"UA-97914674-2"};
